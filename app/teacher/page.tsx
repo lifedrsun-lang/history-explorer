a6e4561018a198022533ed72214598b18c6c3da2
lifedrsun-lang <lifedr.sun@gmail.com>
2026-05-15 14:41:57 +0900

    fix extra closing div

--- app/teacher/page.tsx
+++ app/teacher/page.tsx
@@ -728,10 +728,10 @@ export default function TeacherPage() {
 
                   </div>
 
-                </div>
+         
 
                 <div className="grid grid-cols-2 gap-2">
-
+   
                   <button
                     onClick={() =>
                       addBronze(student)
