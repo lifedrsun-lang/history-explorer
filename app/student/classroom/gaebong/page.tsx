db0363fd81fdfcbab1494ff2d1c22d373c86df8a
lifedrsun-lang <lifedr.sun@gmail.com>
2026-09-11 08:01:01 +0900

    Add dedicated Gaebong classroom route

---
+++ app/student/classroom/gaebong/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import GaebongClassPortal from "../../components/GaebongClassPortal";
+
+export default function GaebongClassroomEntryPage() {
+  return (
+    <GaebongClassPortal
+      onChangeSchool={() => {
+        window.location.href = "/student/history";
+      }}
+    />
+  );
+}
